f2bd5c9eb852c5ffae76b704fce28e179f523b68
Charles Benichou chaffanjon <charlesbenichouchaffanjon@MacBook-Air-de-Charles-2.local>
2024-03-18 20:01:55 +0100

    added the midify_guitarset file : create a folder called midi_data_from_jams with all the solos of GuitarSet transcripted in midi, remove the files before pushing to git

---
+++ src/midify_guitarset.py
@@ -0,0 +1,58 @@
+import requests
+import zipfile
+from io import BytesIO
+import os
+from interpreter import jams_to_midi
+
+# get the jams_file from GuitarSet
+jams_data_url = 'https://zenodo.org/records/3371780/files/annotation.zip?download=1'
+r = requests.get(jams_data_url)
+
+#remove the folder of dataset if it already exists to be sure it's up to date and that we don't have multiple copies of each file
+if not os.path.exists('jams_data'):
+    os.mkdir('jams_data')
+
+
+with zipfile.ZipFile(BytesIO(r.content), 'r') as zip_ref:
+    zip_ref.extractall('jams_data')
+
+
+#keeping only the solo files
+print('removing files that are not solos')
+# Get list of files in the directory
+files = os.listdir('jams_data')
+for file in files:
+    # Check if the file name contains the sequence "solo"
+    if "solo" not in file:
+        # Construct the full file path
+        file_path = os.path.join('jams_data', file)
+        # Remove the file
+        os.remove(file_path)
+print('done')
+
+
+
+# the transcription to midi : this uses jams_to_midi from the GuitarSet git ; 
+#remove the folder of dataset if it already exists to be sure it's up to date and that we don't have multiple copies of each file
+   
+if not os.path.exists('midi_data_from_jams'):
+    os.mkdir('midi_data_from_jams')
+
+files = os.listdir('jams_data')
+jams_files = [f for f in files]
+
+# Convert each JAMS file to MIDI
+for jams_file in jams_files:
+    # Construct paths for input JAMS file and output MIDI file
+    jams_path = os.path.join('jams_data', jams_file)
+    midi_file_name = os.path.splitext(jams_file)[0] + '.mid'
+    midi_file_path = os.path.join('midi_data_from_jams', midi_file_name)
+
+    jams_to_midi(jams_path,title=os.path.splitext(jams_file)[0] + '.mid')  # Convert JAMS to MIDI
+    #reorganize in right folder
+    os.replace(midi_file_name, midi_file_path)  
+
+print("Conversion completed successfully.")
+
+    
+    
