2b3895e4867122e734d8d6faed2d043e676cb0a9
fegounna <yassin.moakher@gmail.com>
2024-03-24 15:39:53 +0100

    training file

---
+++ src/training.py
@@ -0,0 +1,149 @@
+import os
+import torch
+from datasets import load_dataset
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    HfArgumentParser,
+    TrainingArguments,
+    pipeline,
+    logging,
+)
+from peft import LoraConfig, PeftModel
+from trl import SFTTrainer
+
+"""# Define Hyperparameters"""
+
+model_name = "NousResearch/llama-2-7b-chat-hf"
+dataset_name = "./train.jsonl"
+new_model = "llama-2-7b-custom"
+lora_r = 64
+lora_alpha = 16
+lora_dropout = 0.1
+use_4bit = True
+bnb_4bit_compute_dtype = "float16"
+bnb_4bit_quant_type = "nf4"
+use_nested_quant = False
+output_dir = "./results"
+num_train_epochs = 1
+fp16 = False
+bf16 = False
+per_device_train_batch_size = 4
+per_device_eval_batch_size = 4
+gradient_accumulation_steps = 1
+gradient_checkpointing = True
+max_grad_norm = 0.3
+learning_rate = 2e-4
+weight_decay = 0.001
+optim = "paged_adamw_32bit"
+lr_scheduler_type = "constant"
+max_steps = -1
+warmup_ratio = 0.03
+group_by_length = True
+save_steps = 25
+logging_steps = 5
+max_seq_length = None
+packing = False
+device_map = {"": 0}
+
+system_message ="""We will provide you with pieces of music, which are sets of music notes represented by quadruplets, where each variable is separated by ":". Within eacht, the 4 variables are p (pitch), (),duration and (time), each by their value ( example, you may encounter thet p52:v5d1895t212). Here is what each variable to
+- p to the pitch of the note (, p60 to a C3). The pitch difference between2 notes to the ofones that separate them. the velocity note, meaning the intensity or force which the played. d duration note, meaning the duration (in) note be heard. t is the time that separates the instant when the note is played from the instant when the next note will be played. In our syntax, we have arbitrarily chosen, for the last note of each piece, to assign the value -1 to its variable t, in order to detect the end of the respective piece."""
+
+"""#Load Datasets and Train"""
+
+# Load datasets
+train_dataset = load_dataset('json', data_files='../Dataset/train.jsonl', split="train")
+valid_dataset = load_dataset('json', data_files='../Dataset/test.jsonl', split="train")
+
+# Preprocess datasets
+train_dataset_mapped = train_dataset.map(lambda examples: {'text': [f'[INST] <<SYS>>\n{system_message.strip()}\n<</SYS>>\n\n' + prompt + ' [/INST] ' + response for prompt, response in zip(examples['prompt'], examples['response'])]}, batched=True)
+valid_dataset_mapped = valid_dataset.map(lambda examples: {'text': [f'[INST] <<SYS>>\n{system_message.strip()}\n<</SYS>>\n\n' + prompt + ' [/INST] ' + response for prompt, response in zip(examples['prompt'], examples['response'])]}, batched=True)
+
+compute_dtype = getattr(torch, bnb_4bit_compute_dtype)
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=use_4bit,
+    bnb_4bit_quant_type=bnb_4bit_quant_type,
+    bnb_4bit_compute_dtype=compute_dtype,
+    bnb_4bit_use_double_quant=use_nested_quant,
+)
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    quantization_config=bnb_config,
+    device_map=device_map
+)
+model.config.use_cache = False
+model.config.pretraining_tp = 1
+tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.padding_side = "right"
+peft_config = LoraConfig(
+    lora_alpha=lora_alpha,
+    lora_dropout=lora_dropout,
+    r=lora_r,
+    bias="none",
+    task_type="CAUSAL_LM",
+)
+
+training_arguments = TrainingArguments(
+    output_dir=output_dir,
+    num_train_epochs=num_train_epochs,
+    per_device_train_batch_size=per_device_train_batch_size,
+    gradient_accumulation_steps=gradient_accumulation_steps,
+    optim=optim,
+    save_steps=save_steps,
+    logging_steps=logging_steps,
+    learning_rate=learning_rate,
+    weight_decay=weight_decay,
+    fp16=fp16,
+    bf16=bf16,
+    max_grad_norm=max_grad_norm,
+    max_steps=max_steps,
+    warmup_ratio=warmup_ratio,
+    group_by_length=group_by_length,
+    lr_scheduler_type=lr_scheduler_type,
+    report_to="all",
+    evaluation_strategy="steps",
+    eval_steps=5  # Evaluate every 20 steps
+)
+
+trainer = SFTTrainer(
+    model=model,
+    train_dataset=train_dataset_mapped,
+    eval_dataset=valid_dataset_mapped,
+    peft_config=peft_config,
+    dataset_text_field="text",
+    max_seq_length=max_seq_length,
+    tokenizer=tokenizer,
+    args=training_arguments,
+    packing=packing,
+)
+trainer.train()
+trainer.model.save_pretrained(new_model)
+
+# Cell 4: Test the model
+logging.set_verbosity(logging.CRITICAL)
+prompt = f"[INST] <<SYS>>\n{system_message}\n<</SYS>>\n\n<start> p:26:2 t2 p:26:0 t2 p:2c:9 p:33:8 t12 p:2c:0 p:33:0 t13 p:33:7 t1 p:26:5 t12 p:33:0 t1 p:26:0 t1 p:33:6 t3 p:26:3 t9 p:33:0 t3 p:26:0 t10 p:2c:9 t13 p:2c:0 t2 p:26:5 [/INST]" # replace the command here with something relevant to your task
+pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer, max_length=200)
+result = pipe(prompt)
+print(result[0]['generated_text'])
+
+from transformers import pipeline
+
+prompt = f"[INST] <<SYS>>\n{system_message}\n<</SYS>>\n\n<start> p:26:2 t2 p:26:0 t2 p:2c:9 p:33:8 t12 p:2c:0 p:33:0 t13 p:33:7 t1 p:26:5 t12 p:33:0 t1 p:26:0 t1 p:33:6 t3 p:26:3 t9 p:33:0 t3 p:26:0 t10 p:2c:9 t13 p:2c:0 t2 p:26:5 [/INST]" # replace the command here with something relevant to your task
+num_new_tokens = 200
+
+num_prompt_tokens = len(tokenizer(prompt)['input_ids'])
+
+max_length = num_prompt_tokens + num_new_tokens
+
+gen = pipeline('text-generation', model=model, tokenizer=tokenizer, max_length=max_length)
+result = gen(prompt)
+print(result[0]['generated_text'].replace(prompt, ''))
+
+"""#Save the Model"""
+
+#Locally
+trainer.save_model(cwd+"/finetuned_model")
+#to the hub
+model.push_to_hub("lucas0/empath-llama-7b", create_pr=1)
