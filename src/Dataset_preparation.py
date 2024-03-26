646ac2e4f6e2759791588ae034a9128cafebc4ec
Yessin Moakher <130999532+fegounna@users.noreply.github.com>
2024-03-26 17:53:19 +0100

    Update Dataset_preparation.py

--- src/Dataset_preparation.py
+++ src/Dataset_preparation.py
@@ -7,26 +7,30 @@ prompts =  []
 responses = []
 
 directory = "../midis"
+error_counter = 0
 
 for file in tqdm(os.listdir(directory)):
   if file.endswith(".midi") or file.endswith(".mid"):
-    s = texte(directory+'/'+file)
-    n = len(s)
-    if(n>0):
-      i = n//10
-      while(s[i]!=' '):
-        i +=1
-      S1 = s[:i]
-      S2 = s[i+1:]
-
-      prompts.append(S1)
-      responses.append(S2)
-
+    try:
+      s = texte(directory+'/'+file)
+      n = len(s)
+      if(n>0):
+        i = n//10
+        while(s[i]!=' '):
+          i +=1
+        S1 = s[:i]
+        S2 = s[i+1:]
+  
+        prompts.append(S1)
+        responses.append(S2)
+    except Exception as e:
+      error_counter += 1
+      continue
 df = pd.DataFrame({
     'prompt': prompts,
     'response': responses
 })
-
+print("number of errors is ",error_counter)
 train_df = df.sample(frac=0.9, random_state=42)
 test_df = df.drop(train_df.index)
 
