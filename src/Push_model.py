5caea2a9cac21ab2620f2930ce7101e4524301fd
fegounna <yassin.moakher@gmail.com>
2024-03-28 13:11:38 +0100

    DDP Setup

---
+++ src/Push_model.py
@@ -0,0 +1,18 @@
+# Reload model in FP16 and merge it with LoRA weights
+base_model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    low_cpu_mem_usage=True,
+    return_dict=True,
+    torch_dtype=torch.float16,
+    device_map=device_map,
+)
+model = PeftModel.from_pretrained(base_model, output_dir+new_model)
+model = model.merge_and_unload()
+
+# Reload tokenizer to save it
+tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.padding_side = "right"
+
+model.push_to_hub(new_model, use_temp_dir=False)
+tokenizer.push_to_hub(new_model, use_temp_dir=False)
--- src/train.py
+++ src/train.py
@@ -15,43 +15,123 @@ from trl import SFTTrainer
 import wandb
 
 
-
-"""# Define Hyperparameters"""
-
-model_name = "NousResearch/llama-2-7b-chat-hf"
-new_model = "llama-2-7b-music-smidi"
-lora_r = 64
-lora_alpha = 16
-lora_dropout = 0.1
-use_4bit = True
-bnb_4bit_compute_dtype = "float16"
-bnb_4bit_quant_type = "nf4"
-use_nested_quant = False
-output_dir = "/Data/Models/"
-num_train_epochs = 1
-fp16 = False
-bf16 = False
-per_device_train_batch_size = 4
-per_device_eval_batch_size = 4
-gradient_accumulation_steps = 1
-gradient_checkpointing = True
-max_grad_norm = 0.3
-learning_rate = 2e-4
-weight_decay = 0.001
-optim = "paged_adamw_32bit"
-lr_scheduler_type = "constant"
-max_steps = -1
-warmup_ratio = 0.03
-group_by_length = True
-save_steps = 25
-logging_steps = 5
-max_seq_length = None
-packing = False
-device_map = {"": 0}
+#For DDP
+from accelerate import PartialState
+import torch.multiprocessing as mp
+#from torch .utils.data.distributed  import DistributedSampler
+#from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.distributed import init_process_group, destroy_process_group
 
 
 
-wandb.init(
+def ddp_setup(rank,world_size):
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '12355'
+    torch.cuda.set_device(rank)
+    init_process_group(backend='nccl', rank=rank, world_size=world_size)
+
+def main(rank: int, world_size: int):
+    
+    #Put this in Yaml File
+    """# Define Hyperparameters"""
+    model_name = "NousResearch/llama-2-7b-chat-hf"
+    new_model = "llama-2-7b-music-smidi"
+    lora_r = 64
+    lora_alpha = 16
+    lora_dropout = 0.1
+    use_4bit = True
+    bnb_4bit_compute_dtype = "float16"
+    bnb_4bit_quant_type = "nf4"
+    use_nested_quant = False
+    output_dir = "/Data/Models/"
+    num_train_epochs = 1
+    fp16 = False
+    bf16 = False
+    per_device_train_batch_size = 4
+    per_device_eval_batch_size = 4
+    gradient_accumulation_steps = 1
+    gradient_checkpointing = True
+    max_grad_norm = 0.3
+    learning_rate = 2e-4
+    weight_decay = 0.001
+    optim = "paged_adamw_32bit"
+    lr_scheduler_type = "constant"
+    max_steps = -1
+    warmup_ratio = 0.03
+    group_by_length = True
+    save_steps = 25
+    logging_steps = 5
+    max_seq_length = None
+    packing = False
+    device_map = {"": 0}
+
+
+    # You should do this once 
+    system_message ="""A piece of music is a set of music notes that are represented by quadruplets. Within each, the 4 variables(separated by ":") are p (pitch), d (duration),v (velocity) and t (time), followed by their value (example p52:v5:d1895:t212). 
+    p corresponds to the pitch of the note (example p60 for a C3). The pitch difference between 2 notes is the number of semitones that separate them / v the velocity note, the volume of the played note / d duration note, the duration (in milliseconds) of the note to be heard / t the time that separates the instant when the note is played from the instant when the next note will be played.
+    Your job is to complete the composition of """
+
+    """#Load Datasets and Train"""
+
+    # Load datasets
+    train_dataset = load_dataset('json', data_files='./Dataset/train.jsonl', split="train")
+    valid_dataset = load_dataset('json', data_files='./Dataset/test.jsonl', split="train")
+
+    # Preprocess datasets
+    train_dataset_mapped = train_dataset.map(lambda examples: {'text': [f'[INST] <<SYS>>\n{system_message.strip()}\n<</SYS>>\n\n' + prompt + ' [/INST] ' + response for prompt, response in zip(examples['prompt'], examples['response'])]}, batched=True)
+    valid_dataset_mapped = valid_dataset.map(lambda examples: {'text': [f'[INST] <<SYS>>\n{system_message.strip()}\n<</SYS>>\n\n' + prompt + ' [/INST] ' + response for prompt, response in zip(examples['prompt'], examples['response'])]}, batched=True)
+
+    ####################################################
+
+    compute_dtype = getattr(torch, bnb_4bit_compute_dtype)
+    #QLORA config
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=use_4bit,
+        bnb_4bit_quant_type=bnb_4bit_quant_type,
+        bnb_4bit_compute_dtype=compute_dtype,
+        bnb_4bit_use_double_quant=use_nested_quant,
+    )
+    #####################
+
+    #model = DDP(model, device_ids=[device_map]) #For DDP
+
+    #LORA config
+    peft_config = LoraConfig(
+        lora_alpha=lora_alpha,
+        lora_dropout=lora_dropout,
+        r=lora_r,
+        bias="none",
+        task_type="CAUSAL_LM",
+    )
+
+    training_arguments = TrainingArguments(
+        output_dir=output_dir,
+        num_train_epochs=num_train_epochs,
+        per_device_train_batch_size=per_device_train_batch_size,
+        gradient_accumulation_steps=gradient_accumulation_steps,
+        optim=optim,
+        save_steps=save_steps,
+        logging_steps=logging_steps,
+        learning_rate=learning_rate,
+        weight_decay=weight_decay,
+        fp16=fp16,
+        bf16=bf16,
+        max_grad_norm=max_grad_norm,
+        max_steps=max_steps,
+        warmup_ratio=warmup_ratio,
+        group_by_length=group_by_length,
+        lr_scheduler_type=lr_scheduler_type,
+        evaluation_strategy="steps",
+        eval_steps=5,  # Evaluate every 20 steps
+        report_to="wandb",
+        seed=42,
+        gradient_checkpointing_kwargs={'use_reentrant':False}, #For DDP
+    )
+
+
+
+
+    wandb.init(
     project="llm_training",
     config={
         "model_name": model_name,
@@ -64,113 +144,46 @@ wandb.init(
         "optim": optim,
         "weight_decay": weight_decay,
     }
-)
+    )
 
+    ddp_setup(rank,world_size)
 
+    device_string = PartialState().process_index #For DDP
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name,
+        quantization_config=bnb_config,
+        #device_map=device_map #without DDP
+        device_map={'':device_string}, #For DDP
+    )
+    model.config.use_cache = False
+    model.config.pretraining_tp = 1
 
-system_message ="""A piece of music is a set of music notes that are represented by quadruplets. Within each, the 4 variables(separated by ":") are p (pitch), d (duration),v (velocity) and t (time), followed by their value (example p52:v5:d1895:t212). 
-p corresponds to the pitch of the note (example p60 for a C3). The pitch difference between 2 notes is the number of semitones that separate them / v the velocity note, the volume of the played note / d duration note, the duration (in milliseconds) of the note to be heard / t the time that separates the instant when the note is played from the instant when the next note will be played.
-Your job is to complete the composition of """
+    #Load Tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.padding_side = "right"
+    trainer = SFTTrainer(
+        model=model,
+        train_dataset=train_dataset_mapped,
+        eval_dataset=valid_dataset_mapped,
+        peft_config=peft_config,
+        dataset_text_field="text",
+        max_seq_length=max_seq_length,
+        tokenizer=tokenizer,
+        args=training_arguments,
+        packing=packing,
+    )
+    trainer.train()
+    trainer.model.save_pretrained(output_dir+new_model)
+    destroy_process_group() #For DDP
+    wandb.finish()
+    # Empty VRAM
+    del model
+    del trainer
+    import gc
+    gc.collect()
+    gc.collect()
 
-"""#Load Datasets and Train"""
-
-# Load datasets
-train_dataset = load_dataset('json', data_files='./Dataset/train.jsonl', split="train")
-valid_dataset = load_dataset('json', data_files='./Dataset/test.jsonl', split="train")
-
-# Preprocess datasets
-train_dataset_mapped = train_dataset.map(lambda examples: {'text': [f'[INST] <<SYS>>\n{system_message.strip()}\n<</SYS>>\n\n' + prompt + ' [/INST] ' + response for prompt, response in zip(examples['prompt'], examples['response'])]}, batched=True)
-valid_dataset_mapped = valid_dataset.map(lambda examples: {'text': [f'[INST] <<SYS>>\n{system_message.strip()}\n<</SYS>>\n\n' + prompt + ' [/INST] ' + response for prompt, response in zip(examples['prompt'], examples['response'])]}, batched=True)
-
-compute_dtype = getattr(torch, bnb_4bit_compute_dtype)
-#QLORA config
-bnb_config = BitsAndBytesConfig(
-    load_in_4bit=use_4bit,
-    bnb_4bit_quant_type=bnb_4bit_quant_type,
-    bnb_4bit_compute_dtype=compute_dtype,
-    bnb_4bit_use_double_quant=use_nested_quant,
-)
-model = AutoModelForCausalLM.from_pretrained(
-    model_name,
-    quantization_config=bnb_config,
-    device_map=device_map
-)
-model.config.use_cache = False
-model.config.pretraining_tp = 1
-tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-tokenizer.pad_token = tokenizer.eos_token
-tokenizer.padding_side = "right"
-
-#LORA config
-peft_config = LoraConfig(
-    lora_alpha=lora_alpha,
-    lora_dropout=lora_dropout,
-    r=lora_r,
-    bias="none",
-    task_type="CAUSAL_LM",
-)
-
-training_arguments = TrainingArguments(
-    output_dir=output_dir,
-    num_train_epochs=num_train_epochs,
-    per_device_train_batch_size=per_device_train_batch_size,
-    gradient_accumulation_steps=gradient_accumulation_steps,
-    optim=optim,
-    save_steps=save_steps,
-    logging_steps=logging_steps,
-    learning_rate=learning_rate,
-    weight_decay=weight_decay,
-    fp16=fp16,
-    bf16=bf16,
-    max_grad_norm=max_grad_norm,
-    max_steps=max_steps,
-    warmup_ratio=warmup_ratio,
-    group_by_length=group_by_length,
-    lr_scheduler_type=lr_scheduler_type,
-    evaluation_strategy="steps",
-    eval_steps=5,  # Evaluate every 20 steps
-    report_to="wandb",
-    seed=42,
-)
-
-trainer = SFTTrainer(
-    model=model,
-    train_dataset=train_dataset_mapped,
-    eval_dataset=valid_dataset_mapped,
-    peft_config=peft_config,
-    dataset_text_field="text",
-    max_seq_length=max_seq_length,
-    tokenizer=tokenizer,
-    args=training_arguments,
-    packing=packing,
-)
-trainer.train()
-wandb.finish()
-
-trainer.model.save_pretrained(output_dir+new_model)
-
-# Empty VRAM
-del model
-del trainer
-import gc
-gc.collect()
-gc.collect()
-
-# Reload model in FP16 and merge it with LoRA weights
-base_model = AutoModelForCausalLM.from_pretrained(
-    model_name,
-    low_cpu_mem_usage=True,
-    return_dict=True,
-    torch_dtype=torch.float16,
-    device_map=device_map,
-)
-model = PeftModel.from_pretrained(base_model, output_dir+new_model)
-model = model.merge_and_unload()
-
-# Reload tokenizer to save it
-tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-tokenizer.pad_token = tokenizer.eos_token
-tokenizer.padding_side = "right"
-
-model.push_to_hub(new_model, use_temp_dir=False)
-tokenizer.push_to_hub(new_model, use_temp_dir=False)
+if __name__ == "__main__":
+    world_size = torch.cuda.device_count()
+    mp.spawn(main, args=(world_size,), nprocs=world_size)
