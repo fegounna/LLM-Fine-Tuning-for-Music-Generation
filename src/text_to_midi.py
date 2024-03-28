5dd551d76e5d19f8d481004341444afa85112a6e
EchoSlash <42569872+EchoSlash@users.noreply.github.com>
2024-03-28 16:05:16 +0100

    Update text_to_midi.py
    Switch to using py_midicsv instead of mido

--- src/text_to_midi.py
+++ src/text_to_midi.py
@@ -1,24 +1,8 @@
-import mido
-from mido import MidiFile, MidiTrack, Message, MetaMessage
+from copy import deepcopy
+import py_midicsv as pm
 
-with open("../generated files/GMP_s_midis/A., Jag, Je t'aime Juliette, OXC7Fd0ZN8o_augmented_1.txt", "r") as file:
-    s = file.read()[:-1]
 
-print(s)
-def create_midi_file(lieu,simplified_midi):
-    fichier = MidiFile()
-
-    track0 = MidiTrack()
-    trackinit = MidiTrack()
-    fichier.tracks.append(trackinit)
-    fichier.tracks.append(track0)
-    trackinit.append(MetaMessage('set_tempo', tempo=100000, time=0))
-    trackinit.append(MetaMessage('time_signature', numerator=4, denominator=4, clocks_per_click=24, notated_32nd_notes_per_beat=8, time=0))
-#    track0.append(Message('program_change', channel=0 ,program=0, time=0))
-#    track0.append(Message('program_change', channel=0 ,program=0, time=0))
-#    track0.append(MetaMessage('instrument_name', name='acoustic grand', time=0))
-#    track0.append(Message('control_change' ,channel=0 ,control=7 ,value=100 ,time=0))
-#    track0.append(MetaMessage('key_signature', key='C', time=0))
+def create_midi_file(path, simplified_midi, header):
     l = simplified_midi.split(' ')
     
     for i in range(len(l)):
@@ -30,17 +14,37 @@ def create_midi_file(lieu,simplified_midi):
     for i in range (len(l)):
         l[i].append(t_abs)
         t_abs+= l[i][3]
-    j=list(l)
+    j=deepcopy(l)
 
     for a in j:
         l.append([a[0],0,0,0,a[4]+a[2]])
     l = sorted(l, key=lambda x: x[4])
     l.append([0,0,0,0,l[-1][4]])
-    #print(l) 
-    for i in range(len(l)-1):
-        track0.append(Message('note_on', note=l[i][0], velocity=l[i][1], time=l[i+1][4]-l[i][4]))
-    fichier.save(lieu)
+    #print(l) #ici l contient pitch, velocity, duration, time, t_abs dans cet ordre
+
+    csv_string = header
+    for i in range(len(l)):
+        csv_string.append("2, " + str(l[i][4]) + ", Note_on_c, 0, " + str(l[i][0]) + ", " + str(l[i][1]) + "\n")
+    csv_string.append("2, " + str(l[len(l)-1][4]) + ", End_track\n")
+    csv_string.append("0, 0, End_of_file")
+
+    # with open("./csv_string.txt", "w") as file :
+    #     file.write(str(csv_string))
+
+    midi_object = pm.csv_to_midi(csv_string)
+
+    with open(path, "wb") as output_file :
+        midi_writer = pm.FileWriter(output_file)
+        midi_writer.write(midi_object)
 
 
-lieu = r"../generated files/TEST(1).mid"
-create_midi_file(lieu,s)
+#For testing
+path = "example_converted.mid"
+with open("./test.txt", "r") as file:
+    s = file.read()[:-1]
+
+
+# All files in GMP use the same header
+header = ["0, 0, Header, 1, 2, 384\n", "1, 0, Start_track\n", "1, 0, Tempo, 500000\n", "1, 0, Time_signature, 4, 2, 24, 8\n", "1, 1, End_track\n", "2, 0, Start_track\n"]
+
+create_midi_file(path, s, header)
