75d0f9b90e882ca9ccebdea9b6462e41857a616d
Yessin Moakher <130999532+fegounna@users.noreply.github.com>
2024-04-11 17:09:44 +0200

    Update train_ddp_accelerate.py

--- src/train_ddp_accelerate.py
+++ src/train_ddp_accelerate.py
@@ -31,7 +31,7 @@ def main():
     use_nested_quant = False
     output_dir = "/users/eleves-a/2022/yessin.moakher/output/"
     num_train_epochs = 1
-    fp16 = False
+    fp16 = True
     bf16 = False
     per_device_train_batch_size = 4
     gradient_accumulation_steps = 1
