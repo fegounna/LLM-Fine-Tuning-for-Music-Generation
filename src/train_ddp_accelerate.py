256fc265189968c49b926b3b3c4540bbbe6b8873
Yessin Moakher <130999532+fegounna@users.noreply.github.com>
2024-04-13 12:00:38 +0200

    Update train_ddp_accelerate.py

--- src/train_ddp_accelerate.py
+++ src/train_ddp_accelerate.py
@@ -48,7 +48,7 @@ def main():
     group_by_length = True
     save_steps = 0
     logging_steps = 25
-    "max_seq_length = 3048
+    max_seq_length = 3048
     packing = False
     device_map = {"": 0}
 
