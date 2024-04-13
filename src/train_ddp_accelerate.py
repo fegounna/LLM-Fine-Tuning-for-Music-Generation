fd47b4d3e7694f16d6191d90a35ae82e9f54989a
Yessin Moakher <130999532+fegounna@users.noreply.github.com>
2024-04-13 10:59:16 +0200

    Update train_ddp_accelerate.py

--- src/train_ddp_accelerate.py
+++ src/train_ddp_accelerate.py
@@ -97,6 +97,7 @@ def main():
         quantization_config=bnb_config,
         #device_map={'':device_string},
         #device_map = {"": 0},
+        attn_implementation="flash_attention_2",
     )
     model.config.use_cache = False
     model.config.pretraining_tp = 1
