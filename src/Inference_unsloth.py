89907ef7c2a5602cb30207dbd6c0dba2b8f0ae1e
Yessin Moakher <130999532+fegounna@users.noreply.github.com>
2024-04-20 21:47:29 +0200

    Update Inference_unsloth.py

--- src/Inference_unsloth.py
+++ src/Inference_unsloth.py
@@ -23,8 +23,7 @@ system_message ="""You are a classical pianist composer. In this context, each m
 Your Task is to complete the generation of :"""
 
 
-s = """p45:v73:d389:t1 p84:v84:d388:t7 p76:v62:d227:t0 p48:v63:d5:t110 p52:v69:d394:t5 p81:v78:d389:t117 p76:v68:d157:t5 p57:v60:d397:t154 p84:v87:d405:t1 p45:v79:d404:t1 p76:v67:d241:t121 p81:v79:d289:t1 p52:v60:d404:t123 p57:v69:d403:t3 p76:v73:d162:t159 p84:v90:d403:t1"""
-
+s ="p45:v73:d389:t1 p84:v84:d388:t7 p76:v62:d227:t0 p48:v63:d5:t110 p52:v69:d394:t5 p81:v78:d389:t117"
 prompt = f"[INST] <<SYS>>\n{system_message}\n<</SYS>>\n\n{s}[INST]" # replace the ????
 
 inputs = tokenizer([prompt], return_tensors = "pt").to("cuda")
@@ -32,6 +31,6 @@ inputs = tokenizer([prompt], return_tensors = "pt").to("cuda")
 outputs = model.generate(**inputs, max_new_tokens = 2048, use_cache = True)
 print(tokenizer.batch_decode(outputs))
 
-#pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer,penalty_alpha=0.7, top_k=10, max_length=4096)
+#pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer,penalty_alpha=0.9, top_p=0.6, max_length=2048)
 #result = pipe(prompt)
 #print(result[0]['generated_text'])
