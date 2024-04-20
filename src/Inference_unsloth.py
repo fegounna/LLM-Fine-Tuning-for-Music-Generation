60d7b1a83efdcd72c93909a8f175c939310b10ea
Yessin Moakher <130999532+fegounna@users.noreply.github.com>
2024-04-21 01:45:31 +0200

    Update Inference_unsloth.py

--- src/Inference_unsloth.py
+++ src/Inference_unsloth.py
@@ -32,7 +32,8 @@ prompt = f"[INST] <<SYS>> {system_message} <</SYS>> {s} [/INST]"
 #outputs = model.generate(**inputs, max_new_tokens = 512, use_cache = True)
 #print(tokenizer.batch_decode(outputs))
 
-pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer,penalty_alpha=0.6, top_k=5, max_length=1028)
-                #,repetition_penalty=0.5)
+pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer, temperature =0.8,repetition_penalty=0.5,max_length=1028)
+                #penalty_alpha=0.6, max_length=1028)
+                #
 result = pipe(prompt)
 print(result[0]['generated_text'])
