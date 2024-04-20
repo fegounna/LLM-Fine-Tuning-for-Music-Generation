b7c51ee8be47023eb6576327821e98fa195a2470
Yessin Moakher <130999532+fegounna@users.noreply.github.com>
2024-04-21 01:05:59 +0200

    Update Inference_unsloth.py

--- src/Inference_unsloth.py
+++ src/Inference_unsloth.py
@@ -23,7 +23,7 @@ logging.set_verbosity(logging.CRITICAL)
 #Your Task is to complete the generation of :"""
 
 system_message ="""You are a classical pianist composer. In this context, each music note in a musical sequence is described using four parameters: pitch (p) from 0 to 127 (highest pitch), volume (v) from 0 to 127 (loudest), duration of the note (d) in ticks, and the length of the pause (t) in ticks before the next note begins regardless of the previous note's duration. A tick is approximately 5.21 milliseconds. Each parameter is followed by its value and separated by colons (e.g. p52:v57:d195:t212). Your composition should demonstrate a clear progression and development, appropriate pauses, including thoughtful variations in melody, harmony, rhythm. Your Task is to complete the generation of :"""
-s="54:v68:d273:t4 p66:v61:d262:t270 p44:v63:d272:t8 p56:v63:d275:t17 p100:v41:d5:t43 p99:v50:d8:t8 p97:v44:d8:t196 p45:v64:d541:t2"
+s="p57:v77:d73:t125 p59:v55:d43:t98 p60:v84:d817:t265 p62:v57:d45:t232 p64:v73:d69:t227 p66:v72:d103:t259 p59:v69:d384:t7 p67:v70:d377:t461 p62:v72:d37:t1"
 #prompt = f"[INST] <<SYS>>\n{system_message}\n<</SYS>>\n\n{s}[INST]" # replace the ????
 prompt = f"[INST] <<SYS>> {system_message} <</SYS>> {s} [/INST]" 
 
