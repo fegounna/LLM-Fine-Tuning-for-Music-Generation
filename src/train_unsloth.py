d93ee4344d2598b0ebb403da8772cb414e962f77
Yessin Moakher <130999532+fegounna@users.noreply.github.com>
2024-04-20 09:04:41 +0200

    Update train_unsloth.py

--- src/train_unsloth.py
+++ src/train_unsloth.py
@@ -60,8 +60,13 @@ def main():
     )
 
     dataset = load_dataset(dataset_name, split="train")
-    tokenizer.pad_token = tokenizer.eos_token
+    
+    tokenizer.add_special_tokens({'pad_token': '[PAD]'})
     tokenizer.padding_side = "right"
+    EOS_TOKEN = tokenizer.eos_token
+    def formatting_func(example):
+        return example["text"] + EOS_TOKEN
+        
     training_arguments = TrainingArguments(
         output_dir=output_dir,
         num_train_epochs=num_train_epochs,
@@ -92,6 +97,7 @@ def main():
         tokenizer=tokenizer,
         args=training_arguments,
         packing=packing,
+        formatting_func = formatting_func,
     )
     trainer.train()
     trainer.model.save_pretrained(output_dir+new_model)
