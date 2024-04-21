21217aa4db0407bb912baa29a4f9673d680cb501
Yessin Moakher <130999532+fegounna@users.noreply.github.com>
2024-04-21 02:14:48 +0200

    Update Inference_unsloth.py

--- src/Inference_unsloth.py
+++ src/Inference_unsloth.py
@@ -23,7 +23,8 @@ logging.set_verbosity(logging.CRITICAL)
 #Your Task is to complete the generation of :"""
 
 system_message ="""You are a classical pianist composer. In this context, each music note in a musical sequence is described using four parameters: pitch (p) from 0 to 127 (highest pitch), volume (v) from 0 to 127 (loudest), duration of the note (d) in ticks, and the length of the pause (t) in ticks before the next note begins regardless of the previous note's duration. A tick is approximately 5.21 milliseconds. Each parameter is followed by its value and separated by colons (e.g. p52:v57:d195:t212). Your composition should demonstrate a clear progression and development, appropriate pauses, including thoughtful variations in melody, harmony, rhythm. Your Task is to complete the generation of :"""
-s="p48:v66:d272:t1 p84:v80:d272:t0 p76:v63:d272:t116 p52:v69:d156:t1 p81:v76:d155:t121 p45:v70:d42:t1 p84:v82:d41:t1 p76:v63:d41:t114 p81:v79:d40:t0 p52:v69:d40:t1 p48:v66:d39:t1 p76:v61:d39:t117 p84:v81:d37:t1 p52:v68:d38:t1 p48:v65:d37:t0 p76:v62:d37:t115 p81:v77:d39:t0 p52:v67:d38:t2 p48:v63:d37:t1 p76:v61:d37:t116 p84:v80:d36:t1 p52:v67:d36:t1 p48:v63:d35:t1 p76:v60:d35:t117 p81:v76:d37:t0 p52:v66:d37:t1 p48:v63:d37:t1 p76:v60:d37:t117 p84:v80:d36:t1 p52:v67:d35:t0 p48:v63:d35:t1 p76:v61:d35:t117 p81:v77:d36:t0 p52:v67:d35:t0 p48:v63:d35:t1 p76:v60:d35:t117 p84:v81:d35:t1 p52:v68:d34:t1 p48:v63:d34:t0 p76:v60:d34:t118 p81:v76:d35:t0 p52:v67:d35:t1 p48:v63:d35:t1 p76:v60:d34:t117 p84:v80:d34:t0 p52:v66:d34:t1 p48:v62:d33:t0 p76:v61:d33:t118 p81:v76:d34:t0 p52:v66:d34:t1 p48:v63:d34:t1 p76:v60:d34:t117 p84:v81:d34:t0 p52:v67:d34:t1 p48:v63:d33:t1 p76:v61:d33:t117 p81:v76:d34:t0 p52:v67:d33:t1 p48:v63:d33:t1 p76:v61:d33:t117 p84:v81:d33:t1 p52:v67:d33:t0 p48:v63:d32:t1 p76:v60:d32:t118 p81:v76:d33:t1 p52:v66:d32:t0 p48:v62:d32:t1 p76:v60:d32:t118 p84:v81:d32:t1 p52:v66:d32:t1 p48:v63:d31:t1 p76:v61:d31:t118 p81:v76:d33:t0 p52:v66:d33:t1 p48:v62:d33:t1 p76:v60:d32:t118 p84:v81:d34:t1 p52:v66:d33:t1 p48:v62:d32:t0 p76:v60:d32:t118 p81:v76:d33:t0 p52:v66:d33:t1 p48:v62:d32:t1 p76:v60:d32:t117 p84:v81:d32:t1 p52:v66:d31:t1 p48:v62:d30:t0 p76:v60:d30:t118 p81:v76:d32:t1 p52:v66:d31:t0 p48:v62:d31:t1 p76:v60:d31:t118 p84:v81:d31:t1 p52:v66:d30:t1 p48:v62:d29:t1 p76:v60:d29:t117 p81:v76:d31:t1 p52:v66:d30:t1 p48:v62:d29:t1 p76:v60:d29:t117 p84:v81:d31:t1 p52:v66:d30:t0 p48:v62:d29:t1 p76:v60:d29:t117 p81:v76:d31:t1 p52:v66:d30:t1 p48:v62:d29:t1 p76:v60:d29:t117 p84:v81:d30:t0 p52:v66:d30:t1 p48:v62:d29:t0 p76:v60:d29:t117 p81:v76:d31:t0 p52:v66:d30:t1 p48:v62:d29:t1 p76:v60:d29:t117 p84:v81:d30:t1 p52:v66:d29:t1 p48:v62:d29:t0 p76:v60:d29:t117"
+s="p45:v73:d389:t1 p84:v84:d388:t7 p76:v62:d227:t0 p48:v63:d5:t110 p52:v69:d394:t5 p81:v78:d389:t117"
+s="p45:v73:d389:t1 p84:v84:d388:t7 p76:v62:d227:t0 p48:v63:d5:t110 p52:v69:d394:t5 p81:v78:d389:t117"
 #prompt = f"[INST] <<SYS>>\n{system_message}\n<</SYS>>\n\n{s}[INST]" # replace the ????
 prompt = f"[INST] <<SYS>> {system_message} <</SYS>> {s} [/INST]" 
 
@@ -31,7 +32,7 @@ prompt = f"[INST] <<SYS>> {system_message} <</SYS>> {s} [/INST]"
 
 #outputs = model.generate(**inputs, max_new_tokens = 512, use_cache = True)
 #print(tokenizer.batch_decode(outputs))
-pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer, temperature =0.6 ,top_k=5,max_length=2048)
+pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer, temperature =0.6 ,penalty_alpha=0.6,max_length=2048)
 
 #pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer, temperature =0.9,penalty_alpha=0.6,top_p=0.5,max_length=1024)
                 #,repetition_penalty=1.5
