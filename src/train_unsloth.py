0121a55228dbe90e2e2b4a4574c0224c44a62a78
Yessin Moakher <130999532+fegounna@users.noreply.github.com>
2024-04-22 00:00:57 +0200

    Update train_unsloth.py

--- src/train_unsloth.py
+++ src/train_unsloth.py
@@ -15,7 +15,7 @@ import wandb
 from unsloth import FastLanguageModel
 
 def main():
-    new_model = "llama-2-music_4k_2"
+    new_model = "llama-2-music_4k_2_ep"
     max_seq_length = 4096 # Choose any! We auto support RoPE Scaling internally!
     dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
     load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
