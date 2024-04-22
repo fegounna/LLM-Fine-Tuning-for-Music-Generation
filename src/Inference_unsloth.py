a2d09a9e3a6b1ea6f57ae8c8e23d065fdc7cff41
Yessin Moakher <130999532+fegounna@users.noreply.github.com>
2024-04-22 17:19:27 +0200

    Update Inference_unsloth.py

--- src/Inference_unsloth.py
+++ src/Inference_unsloth.py
@@ -28,7 +28,7 @@ Your Task is to complete the generation of :"""
 s="p45:v73:d389:t1 p84:v84:d388:t7 p76:v62:d227:t0 p48:v63:d5:t110 p52:v69:d394:t5 p81:v78:d389:t117"
 prompt = f"<s>[INST] <<SYS>>\n{system_message}\n<</SYS>>\n\n{s}[/INST]" # replace the ????
 
-pipe = pipeline(task="text-generation", model=model,do_sample=True,tokenizer=tokenizer,temperature=0.85,top_p=0.95,max_length=3048)
+pipe = pipeline(task="text-generation", model=model,do_sample=True,tokenizer=tokenizer,temperature=0.8,top_k=1000,top_p=0.95,max_length=3048)
 
 #pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer, eos_token_id=tokenizer.eos_token_id,temperature =0.9,top_p=0.5,max_length=1024)
                 #,repetition_penalty=1.5
