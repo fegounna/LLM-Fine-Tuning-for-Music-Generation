4e848496e750c1a80dbb3b19e3077521b8d6ce6d
Yessin Moakher <130999532+fegounna@users.noreply.github.com>
2024-04-24 22:06:11 +0200

    Update Inference_unsloth.py

--- src/Inference_unsloth.py
+++ src/Inference_unsloth.py
@@ -3,12 +3,12 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 from peft import PeftConfig, PeftModel
 from unsloth import FastLanguageModel
 
-peft_model_id = '/users/eleves-a/2022/yessin.moakher/output/llama-2-music_4k_2_lr/'
+peft_model_id = '/users/eleves-a/2022/yessin.moakher/output/llama-2-music_8k/'
 device_map = {"": 0}
 
 model, tokenizer = FastLanguageModel.from_pretrained(
     model_name = peft_model_id, # YOUR MODEL YOU USED FOR TRAINING
-    max_seq_length = 4096,
+    max_seq_length = 8192,
     dtype = None,
     load_in_4bit = True,
 )
