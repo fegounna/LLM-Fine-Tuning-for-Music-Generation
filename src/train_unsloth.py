97f05b3bb0470d08317b62ac2d38900ec90514c4
Yessin Moakher <130999532+fegounna@users.noreply.github.com>
2024-05-10 19:33:05 +0200

    Update train_unsloth.py

--- src/train_unsloth.py
+++ src/train_unsloth.py
@@ -36,7 +36,7 @@ def main():
     warmup_ratio = 0.01
     lr_scheduler_type = "cosine"
     weight_decay = 0.001
-    logging_steps = 10
+    logging_steps = 5
     packing = False
 
     model, tokenizer = FastLanguageModel.from_pretrained(
