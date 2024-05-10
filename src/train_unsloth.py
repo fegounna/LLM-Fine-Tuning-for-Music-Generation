8f94b57537e3cd4cdc51023493dcb88966b8667a
Yessin Moakher <130999532+fegounna@users.noreply.github.com>
2024-05-10 18:25:38 +0200

    Update train_unsloth.py

--- src/train_unsloth.py
+++ src/train_unsloth.py
@@ -25,8 +25,8 @@ def main():
     max_steps = -1
     fp16 = not torch.cuda.is_bf16_supported()
     bf16 = torch.cuda.is_bf16_supported()
-    per_device_train_batch_size = 8
-    gradient_accumulation_steps = 4 #last
+    per_device_train_batch_size = 16
+    gradient_accumulation_steps = 32 #last
     optim = "paged_adamw_32bit" #last
     save_steps = 500
     learning_rate = 2e-4
