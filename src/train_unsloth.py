65bbb6fd4121644ed88bfa20fc5cdb4b31b686a2
Yessin Moakher <130999532+fegounna@users.noreply.github.com>
2024-05-10 17:55:41 +0200

    Update train_unsloth.py

--- src/train_unsloth.py
+++ src/train_unsloth.py
@@ -29,11 +29,11 @@ def main():
     gradient_accumulation_steps = 4 #last
     optim = "paged_adamw_32bit" #last
     save_steps = 500
-    learning_rate = 2e-4
+    learning_rate = 5e-4
     max_grad_norm = 0.3
     group_by_length = True
     #warmup_ratio = 0.1
-    warmup_ratio = 0.03
+    warmup_ratio = 0.01
     lr_scheduler_type = "cosine"
     weight_decay = 0.001
     logging_steps = 20
